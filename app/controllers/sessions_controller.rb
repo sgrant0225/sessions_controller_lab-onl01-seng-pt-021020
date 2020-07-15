3f0d99fe8b759501e6ea37fb2525d3c5dda290fc
Sharnell Grant <sharnellgrant@gmail.com>
2020-07-15 23:58:14 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,3 +16,12 @@ class SessionsController < ApplicationController
     redirect_to controller: 'application', action: 'hello'
   end
 end
+
+def create
+    if !params[:name] || params[:name].empty?
+      redirect_to controller: 'sessions', action: 'new'
+    else
+      session[:name] = params[:name]
+      redirect_to controller: 'application', action: 'hello'
+    end
+  end
